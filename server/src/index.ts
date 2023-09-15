5daf63e499f33ce1d0625851d08e434dacd37786
Patrick <patrick.lie9@gmail.com>
2023-09-15 21:23:53 +0200

    updates configs

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello World");
+import express from 'express';
+
+function Main() {
+    const app = express();
+    const port = 3050;
+
+    app.get('/', (req, res) => {
+        res.send('Hello World!')
+    })
+
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`);
+    })
+}
+
+Main();
